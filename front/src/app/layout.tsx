5883b4ab987be741428468bbb41ef83707ca1717
Eduardo Cabrejas <zbdkspcs@gmail.com>
2024-07-23 13:40:29 -0300

    Footer Done

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar/NavBar";
+import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <NavBar />
       <main className={inter.className}>{children}</main>
+      <Footer />
       </body>
     </html>
   );
