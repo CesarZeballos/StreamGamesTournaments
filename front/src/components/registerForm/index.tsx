b045bd5d94ed5fd65b3acbea3de4e823f7397588
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-23 09:39:25 -0300

    form creado

---
+++ front/src/components/registerForm/index.tsx
@@ -0,0 +1,107 @@
+'use client'
+import { useState } from "react"
+import { FormContainer } from "../formContainer"
+import { FourColumsContainer } from "../fourColumsContainer"
+import { IRegisterForm } from "@/interfaces/interfaceRegister"
+
+export const RegisterForm: React.FC = () => {
+    const [data, setData] = useState<IRegisterForm>({
+        name: "",
+        nickname: "",
+        email: "",
+        password: "",
+        country: "",
+        birthdate: ""
+    })
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setData({
+            ...data,
+            [name]: value
+        })
+    }
+
+    return (
+        <form>
+            <h1 className="heanding2 text-white mt-9 mb-16">Register</h1>
+            <FourColumsContainer>
+                <h2 className="heanding4 text-white">Your data</h2>
+                        <FormContainer>
+                            <div className="flex flex-col gap-2 w-fit">
+                                <label className="body text-white">Name</label>
+                                <input type="text"
+                                name="name"
+                                value={data.name}
+                                onChange={handleChange}
+                                className="input"
+                                required
+                                />
+                            </div>
+
+                            <div className="flex flex-col gap-2 w-fit">
+                                <label className="body text-white">Nickname</label>
+                                <input type="text"
+                                name="nickname"
+                                value={data.nickname}
+                                onChange={handleChange}
+                                className="input"
+                                required
+                                />
+                            </div>
+
+                            <div className="flex flex-col gap-2 w-fit">
+                                <label className="body text-white">Email</label>
+                                <input type="text"
+                                name="email"
+                                value={data.email}
+                                onChange={handleChange}
+                                className="input"
+                                required
+                                />
+                            </div>
+
+                            <div className="flex flex-col gap-2 w-fit">
+                                <label className="body text-white">Password</label>
+                                <input type="password"
+                                name="password"
+                                value={data.password}
+                                onChange={handleChange}
+                                className="input"
+                                required
+                                />
+                            </div>
+
+                            <div className="flex flex-col gap-2 w-fit">
+                                <label className="body text-white">Country</label>
+                                <input type="text"
+                                name="country"
+                                value={data.country}
+                                onChange={handleChange}
+                                className="input"
+                                required
+                                />
+                            </div>
+
+                            <div className="flex flex-col gap-2 w-fit">
+                                <label className="body text-white">Birthdate</label>
+                                <input type="date"
+                                name="birthdate"
+                                value={data.birthdate}
+                                onChange={handleChange}
+                                className="input"
+                                required
+                                />
+                            </div>
+
+                            <button className="buttonPrimary mt-7" type="submit">Register</button>
+                        </FormContainer>
+                        <div className="col-span-2">
+                            <div>
+
+                            </div>
+                        </div>
+            </FourColumsContainer>
+        </form>
+    )
+}
