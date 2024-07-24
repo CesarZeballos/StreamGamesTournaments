04d955404b0d13f34f298a663b3778318dbb7421
tomasvalentinotech <vrtomas1@gmail.com>
2024-07-23 22:39:08 -0300

    Added new modules for auth, file upload, games, teams, tournaments, and users. Modified app module and package files. Removed app controller spec file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GamesModule } from './games/games.module';
+import { TeamsModule } from './teams/teams.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
+import { AuthModule } from './auth/auth.module';
+import { TournamentsModule } from './tournaments/tournaments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    GamesModule,
+    TournamentsModule,
+    AuthModule,
+    FileUploadModule,
+    TeamsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
