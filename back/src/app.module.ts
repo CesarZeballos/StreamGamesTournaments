4ddef47fbcdb715707472085120a0208024a1220
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-23 21:47:30 -0300

    bk2

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
