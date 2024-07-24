abd561cbc4a267666cd76c799c55cbcb95d5baf8
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-07-23 21:35:28 -0300

    ayuda al back el salvador de la vida

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+        </body>
       </Providers>
     </html>
   );
