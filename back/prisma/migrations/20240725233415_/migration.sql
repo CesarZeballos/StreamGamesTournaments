27166b151f2d0add59f72802505098eebd8a92ff
tomasvalentinotech <vrtomas1@gmail.com>
2024-07-25 20:35:12 -0300

    arreglo-merge

--- back/prisma/migrations/20240725233415_/migration.sql
+++ back/prisma/migrations/20240725233415_/migration.sql
@@ -8,16 +8,14 @@ CREATE TYPE "Categories" AS ENUM ('amateur', 'professional');
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" VARCHAR(80) NOT NULL,
-    "name" TEXT NOT NULL,
     "nickName" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "birthDate" TIMESTAMP(3) NOT NULL,
-    "country" TEXT NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'user',
-    "urlSelfie" TEXT NOT NULL,
+    "urlSelfie" TEXT,
     "actived" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "teamId" TEXT NOT NULL,
+    "teamId" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -42,6 +40,7 @@ CREATE TABLE "Teams" (
     "name" VARCHAR(25) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "urlAvatar" TEXT NOT NULL,
+    "organizerId" TEXT NOT NULL,
     "tournamentId" TEXT NOT NULL,
 
     CONSTRAINT "Teams_pkey" PRIMARY KEY ("id")
@@ -51,6 +50,7 @@ CREATE TABLE "Teams" (
 CREATE TABLE "Games" (
     "id" TEXT NOT NULL,
     "name" VARCHAR(80) NOT NULL,
+    "urlImage" TEXT NOT NULL,
 
     CONSTRAINT "Games_pkey" PRIMARY KEY ("id")
 );
@@ -70,14 +70,23 @@ CREATE UNIQUE INDEX "Tournament_gameId_key" ON "Tournament"("gameId");
 -- CreateIndex
 CREATE UNIQUE INDEX "Teams_name_key" ON "Teams"("name");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Teams_organizerId_key" ON "Teams"("organizerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Games_name_key" ON "Games"("name");
+
 -- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "User" ADD CONSTRAINT "User_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Teams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Tournament" ADD CONSTRAINT "Tournament_organizerId_fkey" FOREIGN KEY ("organizerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Tournament" ADD CONSTRAINT "Tournament_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Games"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Teams" ADD CONSTRAINT "Teams_organizerId_fkey" FOREIGN KEY ("organizerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Teams" ADD CONSTRAINT "Teams_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
