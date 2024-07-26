807858183c2a5b2ff422ac2eb84c1a2ed6672269
Jeison <Jeisonlopezarroyave@gmail.com>
2024-07-26 16:18:49 -0500

    Integración de .env.example

--- back/src/file-upload/file-upload.controller.ts
+++ back/src/file-upload/file-upload.controller.ts
@@ -6,6 +6,7 @@ import {
 	ParseFilePipe,
 	FileTypeValidator,
 	MaxFileSizeValidator,
+	BadRequestException,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
@@ -18,7 +19,7 @@ import {
 } from '@nestjs/swagger';
 import { FileUploadService } from './file-upload.service';
 
-@ApiTags('file-upload')
+@ApiTags('File-Upload')
 @Controller('uploadfile')
 export class FileUploadController {
 	constructor(private readonly fileUploadService: FileUploadService) {}
@@ -38,26 +39,20 @@ export class FileUploadController {
 			},
 		},
 	})
-	@ApiResponse({
-		status: 201,
-		description: 'Archivo cargado exitosamente.',
-	})
+	@ApiResponse({ status: 201, description: 'Archivo cargado exitosamente.' })
 	@ApiResponse({
 		status: 400,
 		description:
-			'Error al cargar el archivo. El tamaño del archivo puede ser mayor a 200KB o el tipo de archivo no es soportado.',
+			'Error al cargar el archivo. El tamaño del archivo puede ser mayor a 1 MB o el tipo de archivo no es soportado.',
 	})
-	@ApiResponse({
-		status: 415,
-		description: 'Tipo de archivo no soportado.',
-	})
+	@ApiResponse({ status: 415, description: 'Tipo de archivo no soportado.' })
 	@UseInterceptors(FileInterceptor('file'))
 	uploadFile(
 		@UploadedFile(
 			new ParseFilePipe({
 				validators: [
 					new MaxFileSizeValidator({
-						maxSize: 1048576, // 1 MB
+						maxSize: 1048576,
 						message:
 							'El tamaño del archivo debe ser menor o igual a 1 MB.',
 					}),
@@ -69,6 +64,9 @@ export class FileUploadController {
 		)
 		file: Express.Multer.File,
 	) {
+		if (!file) {
+			throw new BadRequestException('No se ha subido ningún archivo');
+		}
 		return this.fileUploadService.uploadFile(file);
 	}
 }
