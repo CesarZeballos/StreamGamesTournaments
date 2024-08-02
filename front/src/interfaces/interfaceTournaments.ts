3f2528f5edc9dfae8f7ac70d1796e9ffb99197ec
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-02 01:26:32 -0300

    update de la interfaz de torneos

--- front/src/interfaces/interfaceTournaments.ts
+++ front/src/interfaces/interfaceTournaments.ts
@@ -6,23 +6,29 @@ export interface ITournament {
     nameTournament: string;
     startDate: string;
     createdAt: string;
-    price: number;
     category: string;
+    organizerId: string;
     gameId: string;
     membersNumber: number;
-    award: string[];
-    urlAvatar: string;
-    description: string;
-    maxMember: number;
     maxTeam: number;
-    organizerId: string;
-    game: {
-        id: string;
-        name: string;
-        urlImage: string;
-    };
+    price: number;
+    urlAvatar: string;
+    award: string[];
+    description: string;
+    state: boolean;
+    game: IGame;
+    players: [];
+    organizer: IUser;
 }
 
+export interface IGame {
+    id: string;
+    name: string;
+    urlImage: string;
+    state: boolean
+}
+
+//interface para el form de agregar equipo
 export interface IAddTeam {
     tournamentId: string
     teamName: string
@@ -30,6 +36,7 @@ export interface IAddTeam {
     members: IUser[]
 }
 
+//interface para mostrar mis torneos en el dashboard user
 export interface IMyTournaments {
     id: string
     nameTournament: string
