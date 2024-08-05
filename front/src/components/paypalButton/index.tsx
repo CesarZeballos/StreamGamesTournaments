e79d78fd999c507cb606af917a1c0cfaa2f58e06
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-05 12:10:25 -0300

    la app buildea bien asi que pusheo los cambios para el deploy

--- front/src/components/paypalButton/index.tsx
+++ front/src/components/paypalButton/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '@/redux/store';
@@ -11,45 +12,61 @@ interface PayPalButtonProps {
 
 const PayPalButton: React.FC<PayPalButtonProps> = ({ data, onSuccess }) => {
   const dispatch = useDispatch<AppDispatch>();
+  const clientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;  // Accede a la variable de entorno
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = `https://www.paypal.com/sdk/js?client-id=AXz-99dwGE8h4C8CxzG8iuB0BMhpu8codITuusodt8yi8CA8J3zOBOxZflOi2-z6bwnLXguvBJ-9qljz&currency=USD`;
-    script.async = true;
-    document.body.appendChild(script);
+    if (!clientId) {
+      console.error('PayPal client ID is missing');
+      return;
+    }
 
-    script.onload = () => {
-      // @ts-ignore
-      paypal.Buttons({
-        createOrder: async () => {
-          try {
-            // Llamar a la acción de Redux para crear la orden
-            const orderId = await dispatch(createOrderSlice(data)).unwrap();
-            return orderId; // Asegúrate de que esta acción devuelva el orderID
-          } catch (error) {
-            console.error('Error creating order:', error);
-            // Manejar el error adecuadamente
-            return '';
+    const loadPayPalScript = () => {
+      const script = document.createElement('script');
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=USD`;
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        // @ts-ignore
+        paypal.Buttons({
+          createOrder: async () => {
+            try {
+              const orderId = await dispatch(createOrderSlice(data)).unwrap();
+              return orderId; // Asegúrate de que esta acción devuelva el orderID
+            } catch (error) {
+              console.error('Error creating order:', error);
+              // Manejar el error adecuadamente
+              return '';
+            }
+          },
+          onApprove: async (data: { orderID: string }) => {
+            console.log("orderId", data.orderID)
+            try {
+              await dispatch(captureOrderSlice(data.orderID)).unwrap();
+              onSuccess(data.orderID);
+            } catch (error) {
+              console.error('Error capturing order:', error);
+              // Manejar el error adecuadamente
+            }
+          },
+          onError: (err: any) => {
+            console.error('PayPal Buttons error:', err);
           }
-        },
-        onApprove: async (data: { orderID: string }) => {
-          console.log("orderId", data.orderID)
-          try {
-            // Llamar a la acción de Redux para capturar el pago
-            await dispatch(captureOrderSlice(data.orderID)).unwrap();
-            onSuccess(data.orderID);
-          } catch (error) {
-            console.error('Error capturing order:', error);
-            // Manejar el error adecuadamente
-          }
-        },
-      }).render('#paypal-button-container');
+        }).render('#paypal-button-container');
+      };
+
+      script.onerror = () => {
+        console.error('PayPal script failed to load.');
+      };
+
+      return () => {
+        document.body.removeChild(script);
+      };
     };
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [data, onSuccess, dispatch]);
+    loadPayPalScript();
+
+  }, [data, onSuccess, dispatch, clientId]);
 
   return <div id="paypal-button-container"></div>;
 };
