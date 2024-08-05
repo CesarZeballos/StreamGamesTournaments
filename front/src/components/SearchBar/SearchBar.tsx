21c105f029f86438e7a8bc03d8434153a7e7328a
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-04 21:47:56 -0300

    limpieza del codigo

--- front/src/components/SearchBar/SearchBar.tsx
+++ front/src/components/SearchBar/SearchBar.tsx
@@ -1,24 +1,18 @@
-import Image from "next/image";
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setFilter } from "@/redux/slices/cardsSlice";
-import allIcon from "../../app/assets/images/icons/all-W.png";
-import csIcon from "../../app/assets/images/icons/cs-W.png";
-import ftIcon from "../../app/assets/images/icons/fortnite-W.png";
-import lolIcon from "../../app/assets/images/icons/lol-W.png";
 
 //icons import
-import { MdGames } from "react-icons/md";
 import { SiLeagueoflegends } from "react-icons/si";
 import { SiCounterstrike } from "react-icons/si";
 import { TbBrandFortnite } from "react-icons/tb";
 import { AppDispatch, RootState } from "@/redux/store";
-import { filterEmun, IFilters } from "@/interfaces/interfaceRedux";
+import { filterEmun } from "@/interfaces/interfaceRedux";
 import { filtered } from "@/redux/thunks/auxiliarSliceThunk";
 
 
 const SearchBar: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const filters = useSelector((state: RootState) => state.tournament.filters.game)
   const handleFilterClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const { name, value } = event.currentTarget;
     dispatch(filtered({
@@ -29,10 +23,6 @@ const SearchBar: React.FC = () => {
 
   return (
     <div className="flex flex-row justify-center p-2 gap-6">
-      {/* <button className="buttonFilter" name="game" value={""} onClick={handleFilterClick}>
-        <MdGames />
-        <p>All Tournaments</p>
-      </button> */}
       <button className="buttonFilter" name="game" value={"CounterStrike Go"} onClick={handleFilterClick}>
         <SiCounterstrike />
         <p>Counter Strike</p>
