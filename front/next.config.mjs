c0717caa051af0be47487f798217b2d813f4f27c
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-06 12:29:34 -0300

    solucion del renderizado de imagenes en el banner

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res-console.cloudinary.com'],
+        domains: ['res.cloudinary.com'],
     }
 };
 
--- front/src/utils/GamesArray.ts
+++ front/src/utils/GamesArray.ts
@@ -4,19 +4,19 @@ export const banners: IGame[] = [
     {
         id: '1',
         name: 'CounterStrike Go',
-        urlImage: 'https://res-console.cloudinary.com/dofwlsemg/thumbnails/v1/image/upload/v1722951332/Y3Nnb19tdW1icmg=/drilldown',
+        urlImage: 'https://res.cloudinary.com/dofwlsemg/image/upload/v1722951332/csgo_mumbrh.jpg',
         description: "Join the battle in this exciting team strategy game, where skills and tactics determine victory in epic 5v5 combat."
     },
     {
         id: '2',
         name: 'Fortnite',
-        urlImage: 'https://res-console.cloudinary.com/dofwlsemg/thumbnails/v1/image/upload/v1722951339/Zm9ydG5pdGVfc2U0dGF6/drilldown',
+        urlImage: 'https://res.cloudinary.com/dofwlsemg/image/upload/v1722951339/fortnite_se4taz.jpg',
         description: "Jump into the action in this vibrant battle royale, build structures, outmaneuver your opponents and be the last one standing in an ever-changing world."
     },
     {
         id: '3',
         name: 'League of Legends',
-        urlImage: 'https://res-console.cloudinary.com/dofwlsemg/thumbnails/v1/image/upload/v1722951355/bG9sX2RmejBidA==/drilldown',
+        urlImage: 'https://res.cloudinary.com/dofwlsemg/image/upload/v1722951355/lol_dfz0bt.png',
         description: "Join the battle in this exciting team strategy game, where skills and tactics determine victory in epic 5v5 combat."
     }
 ]
