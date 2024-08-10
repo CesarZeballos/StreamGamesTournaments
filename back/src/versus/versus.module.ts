167a45b977c8a945df317e142e2e19d105049464
FabriGI <fgi.fa2@hotmail.com>
2024-08-09 23:28:56 -0300

    modificacion de modelos de prisma en base al versus model- logica de batallas entre equipos implementadas falta testear

--- back/src/versus/versus.module.ts
+++ back/src/versus/versus.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VersusController } from './versus.controller';
-// import { VersusService } from './versus.service';
+import { VersusService } from './versus.service';
 
 @Module({
   controllers: [VersusController],
-  // providers: [VersusService]
+  providers: [VersusService]
 })
 export class VersusModule { }
