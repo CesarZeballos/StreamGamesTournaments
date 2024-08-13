d8975aaceb7ef1f13cb5b13e69568b91c84ed6f4
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-13 16:28:50 -0300

    chat step2

--- front/src/components/routerChat/index.tsx
+++ front/src/components/routerChat/index.tsx
@@ -1,10 +1,12 @@
 
 import { ChatBoxglobal } from "../chatBoxGlobal";
 import { ChatSelector } from "../chatSelector";
+import { RootState } from "@/redux/store";
 
 
 
 export const RouterChat = () => {
+    const globalChat = useSelector((state: RootState) => state.chat.globalChat)
     return (
         <div className="grid grid-cols-4">
             <ChatSelector />
