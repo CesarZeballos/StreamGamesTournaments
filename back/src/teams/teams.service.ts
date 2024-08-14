dddf6710a726f643d277ddc9d02071fbeb75c55c
Jeison <Jeisonlopezarroyave@gmail.com>
2024-08-14 13:29:32 -0500

    Transacciones en los módulos "teams" y "versus"

--- back/src/teams/teams.service.ts
+++ back/src/teams/teams.service.ts
@@ -19,8 +19,8 @@ export class TeamsService {
 		private readonly prisma: PrismaService,
 		private readonly mailService: MailService,
 		private readonly notificationService: NotificationsService,
-		private readonly fetchs: Fetchs
-	) { }
+		private readonly fetchs: Fetchs,
+	) {}
 
 	async getAllTeams(page: number, limit: number): Promise<Team[]> {
 		const skip = (page - 1) * limit;
@@ -36,101 +36,150 @@ export class TeamsService {
 
 	async getTeamById(id: string): Promise<Team> {
 		const team = await this.fetchs.FindTeamByUnique({ id });
-		if (!team) throw new NotFoundException(`Team with ID ${id} does not exist`);
+		if (!team)
+			throw new NotFoundException(`Team with ID ${id} does not exist`);
 		return team;
 	}
 
 	async createTeam(createTeamDto: CreateTeamDto): Promise<Team> {
-		const { tournamentId, organizerId, users: userIds, ...teamData } = createTeamDto;
+		const {
+			tournamentId,
+			organizerId,
+			users: userIds,
+			...teamData
+		} = createTeamDto;
 
-		const tournament = await this.fetchs.FindTournamentByUnique(tournamentId);
-		if (!tournament) throw new NotFoundException(`Tournament with id: ${tournamentId} does not exist.`);
-
-		const organizer = await this.fetchs.FindUserByUnique({ id: organizerId });
-		if (!organizer) throw new NotFoundException(`Organizer with id: ${organizerId} does not exist.`);
-
-		const existingUserIds = tournament.teams.flatMap(team => team.users.map(user => user.id));
-		const duplicateUsers = userIds.filter(userId => existingUserIds.includes(userId));
-		if (duplicateUsers.length > 0) throw new ConflictException(`Users with IDs ${duplicateUsers.join(', ')} already exist in the tournament.`);
-
-		const team = await this.prisma.team.create({
-			data: {
-				...teamData,
-				organizerId,
-				tournament: { connect: { id: tournamentId } },
-			},
-		});
-
-		if (userIds.length > 0) {
-			for (const userId of userIds) {
-				const foundUser = await this.fetchs.FindUserByUnique({ id: userId });
-				if (!foundUser) throw new BadRequestException(`User with ID ${userId} not found`);
-
-				await this.prisma.userTeams.create({
-					data: {
-						userId: foundUser.id,
-						nameTeam: team.id,
-					},
-				});
-
-				const notification: NotificationDto = {
-					userId: foundUser.id,
-					tournamentId: tournament.id,
-				};
-				await this.notificationService.createNotification(notification);
-
-				const mailOptions = MailTemplates.registeredUser(
-					foundUser.email,
-					foundUser.nickname,
-					tournament.nameTournament,
-					team.name,
-					organizer.nickname,
+		return this.prisma.$transaction(async (prisma) => {
+			const tournament =
+				await this.fetchs.FindTournamentByUnique(tournamentId);
+			if (!tournament)
+				throw new NotFoundException(
+					`Tournament with id: ${tournamentId} does not exist.`,
 				);
-				await this.mailService.sendMail(mailOptions);
+
+			const organizer = await this.fetchs.FindUserByUnique({
+				id: organizerId,
+			});
+			if (!organizer)
+				throw new NotFoundException(
+					`Organizer with id: ${organizerId} does not exist.`,
+				);
+
+			const existingUserIds = tournament.teams.flatMap((team) =>
+				team.users.map((user) => user.id),
+			);
+			const duplicateUsers = userIds.filter((userId) =>
+				existingUserIds.includes(userId),
+			);
+			if (duplicateUsers.length > 0)
+				throw new ConflictException(
+					`Users with IDs ${duplicateUsers.join(', ')} already exist in the tournament.`,
+				);
+
+			const team = await prisma.team.create({
+				data: {
+					...teamData,
+					organizerId,
+					tournament: { connect: { id: tournamentId } },
+				},
+			});
+
+			if (userIds.length > 0) {
+				for (const userId of userIds) {
+					const foundUser = await this.fetchs.FindUserByUnique({
+						id: userId,
+					});
+					if (!foundUser)
+						throw new BadRequestException(
+							`User with ID ${userId} not found`,
+						);
+
+					await prisma.userTeams.create({
+						data: {
+							userId: foundUser.id,
+							nameTeam: team.id,
+						},
+					});
+
+					const notification: NotificationDto = {
+						userId: foundUser.id,
+						tournamentId: tournament.id,
+					};
+					await this.notificationService.createNotification(
+						notification,
+					);
+
+					const mailOptions = MailTemplates.registeredUser(
+						foundUser.email,
+						foundUser.nickname,
+						tournament.nameTournament,
+						team.name,
+						organizer.nickname,
+					);
+					await this.mailService.sendMail(mailOptions);
+				}
 			}
-		}
 
-		const mailOptionsOrganizer = MailTemplates.registeredTeam(
-			organizer.email,
-			organizer.nickname,
-			tournament.nameTournament,
-			team.name,
-		);
-		await this.mailService.sendMail(mailOptionsOrganizer);
+			const mailOptionsOrganizer = MailTemplates.registeredTeam(
+				organizer.email,
+				organizer.nickname,
+				tournament.nameTournament,
+				team.name,
+			);
+			await this.mailService.sendMail(mailOptionsOrganizer);
 
-		return team;
+			return team;
+		});
 	}
 
 	async updateTeam(updateTeamDto: UpdateTeamDto): Promise<Team> {
 		const { id, users: userIds, ...updateData } = updateTeamDto;
 
-		const team = await this.fetchs.FindTeamByUnique({ id });
-		if (!team) throw new NotFoundException('Team does not exist');
+		return this.prisma.$transaction(async (prisma) => {
+			const team = await this.fetchs.FindTeamByUnique({ id });
+			if (!team) throw new NotFoundException('Team does not exist');
 
-		if (userIds) {
-			for (const newUserId of userIds) {
-				const userExists = await this.fetchs.FindUserByUnique({ id: newUserId });
-				if (!userExists) throw new NotFoundException(`User with ID ${newUserId} does not exist`);
+			if (userIds) {
+				for (const newUserId of userIds) {
+					const userExists = await this.fetchs.FindUserByUnique({
+						id: newUserId,
+					});
+					if (!userExists)
+						throw new NotFoundException(
+							`User with ID ${newUserId} does not exist`,
+						);
 
-				const teamMembers = await this.fetchs.FindTeamByUnique({ id });
-				if (teamMembers && teamMembers.users.length >= 5) throw new ConflictException('The team already has 5 members');
+					const teamMembers = await this.fetchs.FindTeamByUnique({
+						id,
+					});
+					if (teamMembers && teamMembers.users.length >= 5)
+						throw new ConflictException(
+							'The team already has 5 members',
+						);
 
-				const userInTeam = await this.prisma.team.findUnique({
-					where: { id },
-					include: { users: true },
-				});
-				if (userInTeam.users.some(user => user.id === newUserId)) {
-					throw new ConflictException(`User with ID ${newUserId} is already a member of this team`);
+					const userInTeam = await prisma.team.findUnique({
+						where: { id },
+						include: { users: true },
+					});
+					if (
+						userInTeam.users.some((user) => user.id === newUserId)
+					) {
+						throw new ConflictException(
+							`User with ID ${newUserId} is already a member of this team`,
+						);
+					}
 				}
 			}
-		}
 
-		return await this.prisma.team.update({
-			where: { id },
-			data: {
-				...updateData,
-				users: userIds ? { connect: userIds.map(userId => ({ id: userId })) } : undefined,
-			},
+			return await prisma.team.update({
+				where: { id },
+				data: {
+					...updateData,
+					users: userIds
+						? { connect: userIds.map((userId) => ({ id: userId })) }
+						: undefined,
+				},
+			});
 		});
 	}
 
@@ -142,7 +191,10 @@ export class TeamsService {
 			where: { id: teamId },
 			include: { users: true },
 		});
-		if (!userInTeam || !userInTeam.users.some(user => user.id === memberId)) {
+		if (
+			!userInTeam ||
+			!userInTeam.users.some((user) => user.id === memberId)
+		) {
 			throw new NotFoundException('Member is not part of the team');
 		}
 
--- back/src/versus/versus.service.ts
+++ back/src/versus/versus.service.ts
@@ -5,168 +5,201 @@ import { Fetchs } from 'utils/fetch.cb';
 
 @Injectable()
 export class VersusService {
-    constructor(private readonly prisma: PrismaService,
-        private readonly fetchs: Fetchs
-    ) { }
+	constructor(
+		private readonly prisma: PrismaService,
+		private readonly fetchs: Fetchs,
+	) {}
 
-    async createVersus(tournamentId: string) {
-        const tournament = await this.fetchs.FindTournamentByUnique(tournamentId)
+	async createVersus(tournamentId: string) {
+		const tournament =
+			await this.fetchs.FindTournamentByUnique(tournamentId);
 
-        if (!tournament) throw new BadRequestException('Tournament does not exist');
+		if (!tournament)
+			throw new BadRequestException('Tournament does not exist');
 
-        if (tournament.versus.length === 0) {
+		if (tournament.versus.length === 0) {
+			await this.createFirstRound(tournament);
+		} else {
+			await this.createNextRound(tournament);
+		}
+	}
 
-            await this.createFirstRound(tournament);
-        } else {
+	async createFirstRound(tournament: any) {
+		const teams = tournament.teams;
+		const firstRound = this.determineRoundName(tournament.maxTeams);
 
-            await this.createNextRound(tournament);
-        }
-    }
+		let positionId: string | null = tournament.positionBattle.id || null;
 
+		await this.prisma.$transaction(async (prisma) => {
+			if (!positionId) {
+				const position = await prisma.positionsBattle.create({
+					data: {
+						round: firstRound,
+						tournament: { connect: { id: tournament.id } },
+					},
+				});
+				positionId = position.id;
+			}
+			for (let i = 0; i < teams.length; i += 2) {
+				await prisma.versus.create({
+					data: {
+						tournament: { connect: { id: tournament.id } },
+						team1: { connect: { id: teams[i].id } },
+						team2: { connect: { id: teams[i + 1]?.id || null } },
+						round: firstRound,
+						positionBattle: { connect: { id: positionId } },
+					},
+				});
+			}
+		});
+	}
 
-    async createFirstRound(tournament: any) {
-        const teams = tournament.teams;
-        const firstRound = this.determineRoundName(tournament.maxTeams);
+	async createNextRound(tournament: any) {
+		const lastRound = this.getLastRound(tournament);
+		const teamsInLastRound = this.getWinningTeamsFromLastRound(
+			tournament,
+			lastRound,
+		);
 
+		await this.prisma.$transaction(async (prisma) => {
+			if (lastRound === Round.SEMIFINAL) {
+				await this.createThirdPlaceBattle(
+					prisma,
+					tournament,
+					teamsInLastRound,
+				);
+				await this.createFinalRound(
+					prisma,
+					tournament,
+					teamsInLastRound,
+				);
+			} else {
+				const nextRound = this.getNextRoundName(lastRound);
+				await this.createSubsequentRound(
+					prisma,
+					tournament,
+					teamsInLastRound,
+					nextRound,
+				);
+			}
+		});
+	}
 
-        let positionId: string | null = tournament.positionBattle.id || null;
+	async createThirdPlaceBattle(
+		prisma: any,
+		tournament: any,
+		teams: string[],
+	) {
+		const [loser1, loser2] = teams;
 
-        if (!positionId) {
-            const position = await this.prisma.positionsBattle.create({
-                data: {
-                    round: firstRound,
-                    tournament: { connect: { id: tournament.id } },
-                },
-            });
-            positionId = position.id;
-        }
+		await prisma.versus.create({
+			data: {
+				tournament: { connect: { id: tournament.id } },
+				team1: { connect: { id: loser1 } },
+				team2: { connect: { id: loser2 } },
+				round: Round.THIRD_PLACE,
+			},
+		});
+	}
 
-        for (let i = 0; i < teams.length; i += 2) {
-            await this.prisma.versus.create({
-                data: {
-                    tournament: { connect: { id: tournament.id } },
-                    team1: { connect: { id: teams[i].id } },
-                    team2: { connect: { id: teams[i + 1]?.id || null } },
-                    round: firstRound,
-                    positionBattle: { connect: { id: positionId } },
-                },
-            });
-        }
-    }
+	async createFinalRound(prisma: any, tournament: any, teams: string[]) {
+		await prisma.versus.create({
+			data: {
+				tournament: { connect: { id: tournament.id } },
+				team1: { connect: { id: teams[0] } },
+				team2: { connect: { id: teams[1] } },
+				round: Round.FINAL,
+			},
+		});
+	}
 
+	async createSubsequentRound(
+		prisma: any,
+		tournament: any,
+		teams: string[],
+		round: Round,
+	) {
+		let positionId: string | null = tournament.positionBattle.id || null;
 
-    async createNextRound(tournament: any) {
-        const lastRound = this.getLastRound(tournament);
-        const teamsInLastRound = this.getWinningTeamsFromLastRound(tournament, lastRound);
+		if (!positionId) {
+			const position = await prisma.positionsBattle.create({
+				data: {
+					round: round,
+					tournament: { connect: { id: tournament.id } },
+				},
+			});
+			positionId = position.id;
+		}
 
-        if (lastRound === Round.SEMIFINAL) {
+		for (let i = 0; i < teams.length; i += 2) {
+			await prisma.versus.create({
+				data: {
+					tournament: { connect: { id: tournament.id } },
+					team1: { connect: { id: teams[i] } },
+					team2: { connect: { id: teams[i + 1] || null } },
+					round: round,
+					positionBattle: { connect: { id: positionId } },
+				},
+			});
+		}
+	}
 
-            await this.createThirdPlaceBattle(tournament, teamsInLastRound);
+	determineRoundName(maxTeams: number): Round {
+		switch (maxTeams) {
+			case 2:
+				return Round.FINAL;
+			case 4:
+				return Round.SEMIFINAL;
+			case 8:
+				return Round.QUARTER_FINAL;
+			case 16:
+				return Round.ROUND_OF_16;
+			default:
+				return Round.FIRST_ROUND;
+		}
+	}
 
-            await this.createFinalRound(tournament, teamsInLastRound);
-        } else {
+	getLastRound(tournament: any): Round {
+		return tournament.versus.reduce(
+			(max, versus) => (max > versus.round ? max : versus.round),
+			Round.FIRST_ROUND,
+		);
+	}
 
-            const nextRound = this.getNextRoundName(lastRound);
-            await this.createSubsequentRound(tournament, teamsInLastRound, nextRound);
-        }
-    }
+	getWinningTeamsFromLastRound(tournament: any, lastRound: Round): string[] {
+		return tournament.versus
+			.filter((versus) => versus.round === lastRound)
+			.map((versus) => versus.winnerId)
+			.filter(Boolean);
+	}
 
-    async createThirdPlaceBattle(tournament: any, teams: string[]) {
-        const [loser1, loser2] = teams;
+	getNextRoundName(lastRound: Round): Round {
+		const rounds = [
+			Round.FIRST_ROUND,
+			Round.ROUND_OF_16,
+			Round.QUARTER_FINAL,
+			Round.SEMIFINAL,
+			Round.FINAL,
+			Round.THIRD_PLACE,
+		];
+		const currentIndex = rounds.indexOf(lastRound);
+		return rounds[currentIndex + 1] || Round.FINAL;
+	}
 
-        await this.prisma.versus.create({
-            data: {
-                tournament: { connect: { id: tournament.id } },
-                team1: { connect: { id: loser1 } },
-                team2: { connect: { id: loser2 } },
-                round: Round.THIRD_PLACE,
-            },
-        });
-    }
+	async updateWinnerRound(winnerId: string, versusId: string) {
+		const versusData = await this.prisma.versus.findUnique({
+			where: { id: versusId },
+		});
 
-    async createFinalRound(tournament: any, teams: string[]) {
-        await this.prisma.versus.create({
-            data: {
-                tournament: { connect: { id: tournament.id } },
-                team1: { connect: { id: teams[0] } },
-                team2: { connect: { id: teams[1] } },
-                round: Round.FINAL,
-            },
-        });
+		if (!versusData) throw new BadRequestException('Battle does not exist');
 
-    }
+		const versus = await this.prisma.versus.update({
+			where: {
+				id: versusData.id,
+			},
+			data: { winnerId },
+		});
 
-    async createSubsequentRound(tournament: any, teams: string[], round: Round) {
-        let positionId: string | null = tournament.positionBattle.id || null;
-
-        if (!positionId) {
-            const position = await this.prisma.positionsBattle.create({
-                data: {
-                    round: round,
-                    tournament: { connect: { id: tournament.id } },
-                },
-            });
-            positionId = position.id;
-        }
-
-        for (let i = 0; i < teams.length; i += 2) {
-            await this.prisma.versus.create({
-                data: {
-                    tournament: { connect: { id: tournament.id } },
-                    team1: { connect: { id: teams[i] } },
-                    team2: { connect: { id: teams[i + 1] || null } },
-                    round: round,
-                    positionBattle: { connect: { id: positionId } },
-                },
-            });
-        }
-    }
-
-    determineRoundName(maxTeams: number): Round {
-        switch (maxTeams) {
-            case 2:
-                return Round.FINAL;
-            case 4:
-                return Round.SEMIFINAL;
-            case 8:
-                return Round.QUARTER_FINAL;
-            case 16:
-                return Round.ROUND_OF_16;
-            default:
-                return Round.FIRST_ROUND;
-        }
-    }
-
-    getLastRound(tournament: any): Round {
-        return tournament.versus.reduce((max, versus) => max > versus.round ? max : versus.round, Round.FIRST_ROUND);
-    }
-
-    getWinningTeamsFromLastRound(tournament: any, lastRound: Round): string[] {
-        return tournament.versus
-            .filter(versus => versus.round === lastRound)
-            .map(versus => versus.winnerId)
-            .filter(Boolean);
-    }
-
-    getNextRoundName(lastRound: Round): Round {
-        const rounds = [Round.FIRST_ROUND, Round.ROUND_OF_16, Round.QUARTER_FINAL, Round.SEMIFINAL, Round.FINAL, Round.THIRD_PLACE];
-        const currentIndex = rounds.indexOf(lastRound);
-        return rounds[currentIndex + 1] || Round.FINAL;
-    }
-
-    async updateWinnerRound(winnerId: string, versusId: string) {
-        const versusData = await this.prisma.versus.findUnique({ where: { id: versusId } })
-
-        if (!versusData) throw new BadRequestException('Battle does not exist');
-
-        const versus = await this.prisma.versus.update({
-            where: {
-                id: versusData.id
-            },
-            data: { winnerId }
-        })
-
-        return versus
-    }
+		return versus;
+	}
 }
-
