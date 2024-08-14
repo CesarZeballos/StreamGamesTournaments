768961c07f5787402d2081ad3729f559ca2970a7
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-14 20:07:43 -0300

    control de ingreso al chat

--- front/src/components/chatGlobalButton/index.tsx
+++ front/src/components/chatGlobalButton/index.tsx
@@ -2,12 +2,31 @@
 import { useState } from "react";
 import { ChatBoxglobal } from "../chatBoxGlobal";
 import ChatIcon from '@mui/icons-material/Chat';
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const ChatGlobalButton: React.FC = () =>  {
     const [openChat, setOpenChat] = useState(false);
+    const user = useSelector((state: RootState) => state.user.user);
+    const router = useRouter();
 
     const handleChange = () => {
-        setOpenChat(true);
+        if (user?.nickname === undefined) {
+          toast.info("You must be logged in to access the global chat", {
+            position: "top-right",
+            duration: 1500,
+            action: {
+              label: "ok",
+              onClick: () => {
+                router.push("/login");
+              },
+            }
+          })
+        } else {
+          setOpenChat(true);
+        }
     }
     const handleClose = () => {
         setOpenChat(false);
