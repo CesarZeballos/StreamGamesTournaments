7ae3112686465014bb90532f0b03c09607325c5f
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-08-15 12:06:37 -0300

    Update games.dto.ts

--- back/src/games/dto/games.dto.ts
+++ back/src/games/dto/games.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUrl, Length } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateGameDto {
 	@ApiProperty({
